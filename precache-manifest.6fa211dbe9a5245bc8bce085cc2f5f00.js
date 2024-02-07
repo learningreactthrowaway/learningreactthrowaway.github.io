8ebaa51cc6137582351116a5c6beea54071a948c
Serai Yan <dave@codingisart.com>
2024-02-07 01:22:45 -0800

    Updates

---
+++ precache-manifest.6fa211dbe9a5245bc8bce085cc2f5f00.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "239710f613a925cd89d9a9da10bbc719",
+    "url": "/index.html"
+  },
+  {
+    "revision": "04ef3c8728e51dc1eced",
+    "url": "/static/css/main.ac174bd7.chunk.css"
+  },
+  {
+    "revision": "563c2164b639f16c9b67",
+    "url": "/static/js/2.c2f28411.chunk.js"
+  },
+  {
+    "revision": "04ef3c8728e51dc1eced",
+    "url": "/static/js/main.22126f2e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b83ce9c59c73ade26bb7871143fd76bb",
+    "url": "/static/media/Nunito-Regular.b83ce9c5.ttf"
+  },
+  {
+    "revision": "85daf9c50e0de17ce952a7c9bf918bae",
+    "url": "/static/media/PaytoneOne-Regular.85daf9c5.ttf"
+  },
+  {
+    "revision": "ff55f8820f081a201e1aa5c9b277d4ca",
+    "url": "/static/media/background.ff55f882.jpg"
+  },
+  {
+    "revision": "249175dff13573825760d091e5a62de5",
+    "url": "/static/media/catdance.249175df.gif"
+  },
+  {
+    "revision": "6f560891f0d7d53ae11835946a63966f",
+    "url": "/static/media/catvibe.6f560891.gif"
+  },
+  {
+    "revision": "28272775104e577c403195ef70c06bdd",
+    "url": "/static/media/doge.28272775.gif"
+  },
+  {
+    "revision": "c24c33ec873f153b74d14eb17c47a80b",
+    "url": "/static/media/dollar.c24c33ec.png"
+  },
+  {
+    "revision": "9c2536b4bcbe81d8e06fe8c7d2e2f5f4",
+    "url": "/static/media/jar.9c2536b4.png"
+  },
+  {
+    "revision": "cc2e2c15ee9eaaf325b801070165c6b5",
+    "url": "/static/media/powell.cc2e2c15.gif"
+  }
+]);
